55608764d126790c01e03a107a9d0f5b4f4bfaf3
unknown <ac2264koji@PC320.ed.y.kumamoto-nct.ac.jp>
2015-06-03 11:17:10 +0900

    「ゆき」をつくりました

---
+++ onngaku_jun01a/Arduino_music.ino
@@ -0,0 +1,68 @@
+const int loudspeaker_pin = 13;
+const int base_time = 16 * 50;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(loudspeaker_pin, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+tone(loudspeaker_pin, 392);
+delay(base_time * 3 / 2 / 4);
+tone(loudspeaker_pin, 440);
+delay(base_time / 8);
+tone(loudspeaker_pin, 392);
+delay(base_time * 3 / 2 / 4);
+tone(loudspeaker_pin, 440);
+delay(base_time / 8);
+tone(loudspeaker_pin, 392);
+delay(base_time / 2);
+tone(loudspeaker_pin, 330);
+delay(base_time / 4);
+noTone(loudspeaker_pin);
+delay(base_time / 4);
+tone(loudspeaker_pin, 330);
+delay(base_time * 3 / 2 / 4);
+tone(loudspeaker_pin, 349);
+delay(base_time / 8);
+tone(loudspeaker_pin, 330);
+delay(base_time * 3 / 2 / 4);
+tone(loudspeaker_pin, 349);
+delay(base_time / 4);
+tone(loudspeaker_pin, 330);
+delay(base_time / 2);
+tone(loudspeaker_pin, 262);
+delay(base_time / 4);
+noTone(loudspeaker_pin);
+delay(base_time / 8);
+tone(loudspeaker_pin, 330);
+delay(base_time / 2);
+tone(loudspeaker_pin, 262);
+delay(base_time / 4);
+tone(loudspeaker_pin, 294);
+delay(base_time / 4);
+tone(loudspeaker_pin, 330);
+delay(base_time / 2);
+tone(loudspeaker_pin, 262);
+delay(base_time / 4);
+tone(loudspeaker_pin, 294);
+delay(base_time / 4);
+tone(loudspeaker_pin, 330);
+delay(base_time * 3 / 2 / 4);
+tone(loudspeaker_pin, 349);
+delay(base_time / 8);
+tone(loudspeaker_pin, 392);
+delay(base_time / 2);
+tone(loudspeaker_pin, 294);
+delay(base_time * 3 / 2 / 4);
+tone(loudspeaker_pin, 330);
+delay(base_time / 8);
+tone(loudspeaker_pin, 294);
+delay(base_time / 4);
+noTone(loudspeaker_pin);
+delay(base_time / 4);
+
+}
